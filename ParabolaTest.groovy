58f24605ab97076c35d22f77a4687e9c892f1f9c
CAD PC <CAD PC@CAD-Workstation>
2022-03-05 15:14:32 -0500

    parameters to play

--- ParabolaTest.groovy
+++ ParabolaTest.groovy
@@ -1,8 +1,7 @@
-return [ Parabola.extrudeByEquation(5,0.27,0,1)
+return [ Parabola.extrudeByEquation(5,0.27,0,5)
 	.move(20,0,0),
 	Parabola.coneByEquation(5,1.27,0.7)
 	.move(30,0,0),
-	Parabola.coneByFocalLength(10, 10),
-	Parabola.coneByHeight(10, 20)
+	Parabola.coneByHeight(10, 50)
 		.move(50,0,0)
 ]
